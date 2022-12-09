a165c4da471d149150b531a28e33275cc578fd07
rishab1017 <119882969+rishab1017@users.noreply.github.com>
2022-12-10 00:01:01 +0530

    Add files via upload

---
+++ Java Assignment 1/Assign 6- Emppay.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import employee.EMP;
+class Emppay
+{
+
+ public static void main(String args[])
+ {
+
+   EMP e = new EMP("Brandon Sanderson","37","Male",49000);
+   e.call();
+   e.display();
+   }
+ }
