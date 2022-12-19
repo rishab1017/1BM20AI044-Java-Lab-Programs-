1571b72e447f4d2f4ef33d1f67f153a9f99f570e
rishab1017 <119882969+rishab1017@users.noreply.github.com>
2022-12-19 10:24:27 +0530

    Add files via upload

---
+++ Java Assignment 2/Main.java
@@ -0,0 +1,12 @@
+import gcdNum.*;
+import lcmNum.*;
+
+public class Main {
+    public static void main(String[] args) {
+        gcd g = new gcd();
+        g.show();
+
+        lcm l = new lcm();
+        l.show();
+    }
+}
